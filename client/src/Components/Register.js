1ca47231a3b113661322e2042c1cd25bcd3df2ef
Mahadeva G S <gsmahadeva52@gmail.com>
2025-09-18 14:23:42 +0530

    Update Register.js

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -21,7 +21,7 @@ const Register = () => {
 
     try {
       const response = await fetch(
-        "https://recipes-s.onrender.com/auth/register",
+        "https://recipes-1-94o3.onrender.com/auth/register",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
